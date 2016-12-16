6d0d27eeb015bbff2e2840a393f5f174c31b70e5
Zhukovsky <valentin.zhukovsky@teamaol.com>
2016-12-16 12:42:42 +0300

    8

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask('default', function () {
+    console.log('default');
+  });
+
+  grunt.registerTask('prepublish', function () {
+    console.log('prebublish');
+  });
+};
